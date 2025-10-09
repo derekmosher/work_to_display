0c016d2864aa4e38e849dd114d317857c42a9548
derek mosher <derekmosher@gmail.com>
2025-10-08 18:49:51 -0700

    Add files via upload

---
+++ BlueAlley/project_07/600x250/initial.js
@@ -0,0 +1,128 @@
+"use strict";
+window.onload = function() {
+   
+/******************** VARIABLES  ********************/ 
+function politeInit(){		
+        /************** Create VARIABLES ********************/
+        var select = function(s) {
+                return document.querySelector(s);
+            },
+            selectAll = function(s) {
+                return document.querySelectorAll(s);
+            },
+            bannerCover = select('#bannerCover'),
+            border = select('#border'),
+            wrapper = select('#wrapper'),
+            text_sub = select('#text_sub'),
+            text_head = select('#text_head'),
+            text1 = select('#text1'),
+
+
+            logo_blueAlly = select('#logo_blueAlly'),
+            divider = select('#divider'),
+            logo_cisco = select('#logo_cisco'),
+            pic1 = select('#pic1'), 
+            //
+            bg = select('#bg'),
+            bgexit = select('#bgexit'),
+            cta = select('#cta'), 
+            colors ={},
+            overme = 0,
+            tl = gsap.timeline();
+
+            /************** Modify VARIABLES ********************/
+            
+            colors.cta_bg = "#479ad4";
+            colors.cta_bg_over = "#fff";
+            colors.cta_text = "#fff";
+            colors.cta_text_over = "#479ad4";
+            colors.border = "#479ad4";
+            colors.border_over = "479ad4";
+
+/***************** //end of VARIABLES  *****************/
+    
+/******************  MAIN ANIMATION  ******************/ 
+		function animate() { 
+            tl
+            .to(bannerCover, {duration:1, alpha:0, ease:"none"},">0.5")
+
+            .from([pic1],{duration:1.1, alpha:1,ease:"power4.out"}, "<0.3")
+            //
+            .from(logo_cisco, {duration:0.8, alpha: 0, scale:1,ease:"power1.out"}, "<0.4")
+            .from(divider, {duration:0.8, alpha: 0, scale:1,ease:"power1.out"}, "<0.1")
+            .from(logo_blueAlly, {duration:0.8, alpha: 0, scale:1,ease:"power1.out"}, "<0.1")
+            //
+            .from([text_sub],{duration:1.8,alpha:0,ease:"power1.out"}, "<0.3")
+            .from([text_head],{duration:1.8,alpha:0,ease:"power1.out"}, "<0.3")
+            .from(cta, {duration:0.8, alpha: 0, scale:1,ease:"power1.out"}, "<0.3")
+            //
+            //
+
+		}
+        animate()
+       
+/******************  //end of MAIN ANIMATION  ******************/    
+    
+        
+/********************  EVENTS  ********************/ 
+        bgexit.addEventListener('mouseover', (e) => {
+
+            console.log("over bgexit")
+            if(overme==1) return;
+            // gsap.set( text1,{x:"+=102px"})
+            gsap.to(cta, {duration:0.6, 
+                y:"15px",
+                onStart: function(){ overme=1;},
+                // color: colors.cta_text_over,
+                // borderColor: colors.cta_border_over,
+                // background: colors.cta_bg_over,
+                ease:"power1.out"}  
+            ) ;
+            gsap.to([text_head,text_sub], {duration:0.6, 
+                y:"-15px",
+                ease:"power1.out"}  
+            ) ;
+            gsap.to([text1], {duration:0.6, 
+                alpha:1,
+                ease:"power1.out"}  
+            ) ;
+            gsap.fromTo(text1, 
+                { x: "0" }, 
+                { x:"-=530", 
+                duration: 17,
+                ease:"power1.out"   
+            });
+        });
+        bgexit.addEventListener('mouseout', (e) => {
+            console.log("out")
+            gsap.to(cta, {duration:0.6, 
+                y:"0px",
+                onComplete: function(){ overme=0;},
+                // color: colors.cta_text,
+                //  borderColor: colors.cta_border,
+                //  background: colors.cta_bg,
+                 ease:"power1.out"}  
+            ) ;
+            gsap.to([text_head,text_sub], {duration:0.6, 
+                y:"0px",
+                ease:"power1.out"}  
+            ) ;
+            gsap.to([text1], {duration:0.6, 
+                alpha:0,
+                ease:"power1.out"}  
+            ) ;
+            
+        });
+
+        let type = 'click';
+        // // ((Modernizr.touchevents)&&(!isChrome)) ? 'touchend' : 'click',
+        let clickable = selectAll('.clickable');
+
+        clickable.forEach(element => element.addEventListener(type, function(e) {
+            console.log("click")
+            return false;
+        }, false));
+	};
+/****************** //end of  EVENTS  ******************/   
+politeInit()
+}
