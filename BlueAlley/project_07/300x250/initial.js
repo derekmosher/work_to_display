dcb0f779f7f3438e66d6853622f01e4993848f71
derek mosher <derekmosher@gmail.com>
2025-10-10 13:21:36 -0700

    Add files via upload

--- BlueAlley/project_07/300x250/initial.js
+++ BlueAlley/project_07/300x250/initial.js
@@ -88,6 +88,17 @@ function politeInit(){
         }
     }
 
+    let isMobile = false;
+    function isMobileUserAgent() {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent)) {
+          return true;
+          let isMobile = true;
+        }
+        return false;
+    }
+    console.log("Is mobile (User Agent):", isMobileUserAgent());
+
 /********************  EVENTS  ********************/ 
         bgexit.addEventListener('mouseover', (e) => {
             if( bannerAnimationDone == 0)return;
